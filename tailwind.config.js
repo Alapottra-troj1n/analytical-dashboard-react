bc477225972730849e8fc2084395d80065e88b8d
Alapottra <alapottratrojon@gmail.com>
2022-06-02 23:22:41 +0600

    added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FB9678",
+          secondary: "#03C9D7",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 }
